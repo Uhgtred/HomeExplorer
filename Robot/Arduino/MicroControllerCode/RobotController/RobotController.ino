0d77720bb2f5d42cae848af02fe261e646b31e62
Uhgtred <markus_koesters@gmx.de>
2023-03-08 22:44:14 +0100

    adjusting tab-stops to the same level putting some comments correcting a typing-mistake in a variable

--- Robot/Arduino/MicroControllerCode/RobotController/RobotController.ino
+++ Robot/Arduino/MicroControllerCode/RobotController/RobotController.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
 int ServoXPin = 2;
-int ServoZpin = 3;
+int ServoZPin = 3;
 int RMotorFPin = 10;
 int RMotorRPin = 11;
 int LMotorFPin = 12;
@@ -18,7 +18,7 @@ void setup() {
     Serial.begin(9600);
     Serial.setTimeout(50);
     XServo.attach(ServoXPin);
-    ZServo.attach(ServoZpin);
+    ZServo.attach(ServoZPin);
     pinMode(LMotorFPin, OUTPUT);
     pinMode(LMotorRPin, OUTPUT);
     pinMode(RMotorFPin, OUTPUT);
@@ -29,20 +29,20 @@ void setup() {
 }
 
 void loop() {
-  stringData = Serial.readString(); //"TEST,TEST2,TEST3"; Testing-line
-  /*Setting the motorpins low in each iteration. If something gets stock or communication breaks robot will stop!*/
-  digitalWrite(LMotorFPin, LOW);
-  digitalWrite(LMotorRPin, LOW);
-  digitalWrite(RMotorFPin, LOW);
-  digitalWrite(RMotorRPin, LOW);
-  if (stringData.length() != 0){
+    stringData = Serial.readString(); //"TEST,TEST2,TEST3"; Testing-line
+    /*Setting the motor-pins low in each iteration. If something gets stock or communication breaks robot will stop!*/
+    digitalWrite(LMotorFPin, LOW);
+    digitalWrite(LMotorRPin, LOW);
+    digitalWrite(RMotorFPin, LOW);
+    digitalWrite(RMotorRPin, LOW);
+    if (stringData.length() != 0){
     String *arrayData = ToStringArray(stringData);
     Serial.println(arrayData[0] + arrayData[1]);
     MotorControl(arrayData);
     ServoControl(arrayData);
     delete[] arrayData;
-  }
-  delay(50);
+    }
+    delay(50);
 }
 
 String *ToStringArray(String data){
@@ -61,6 +61,7 @@ String *ToStringArray(String data){
     int lastPosition = 0;
     int counter = 0;
     for (int i=0; i <= data.length() - 1; i++){
+        // if a separator has been found, the string will be taken from last separator-position to the new position
         if (data[i] == separator){
             newData[counter] = data.substring(lastPosition, i);
             counter++;
@@ -71,44 +72,51 @@ String *ToStringArray(String data){
 }
 
 void MotorControl(String *arrayData){
-  int RMotorValue = 0;
-  int LMotorValue = 0;
-  RMotorValue = arrayData[0].toInt();
-  LMotorValue = arrayData[2].toInt();
-  if (arrayData[1].toInt() == 1){ //data[1] is bool and decides if RMotor is turning backward or forward
+    /*Sending PWM-signals to the motor-controllers*/
+    int RMotorValue = 0;
+    int LMotorValue = 0;
+    //reading the data from array which is being provided through the serial-connection
+    RMotorValue = arrayData[0].toInt();
+    LMotorValue = arrayData[2].toInt();
+    //data[1] is bool and decides if RMotor is turning clockwise or counterclockwise
+    if (arrayData[1].toInt() == 1){
     analogWrite(RMotorRPin, RMotorValue);
     //Serial.println(RMotorValue);
-  }
-  else{
+    }
+    else{
     analogWrite(RMotorFPin, RMotorValue);
     //Serial.println(RMotorValue);
-  }
-  if (arrayData[3].toInt() == 1){ //data[3] is bool and decides if LMotor is turning backward or forward
+    }
+    //data[3] is bool and decides if LMotor is turning clockwise or counterclockwise
+    if (arrayData[3].toInt() == 1){
     analogWrite(LMotorRPin, LMotorValue);
     //Serial.println(LMotorValue);
-  }
-  else{
+    }
+    else{
     analogWrite(LMotorFPin, LMotorValue);
     //Serial.println(LMotorValue);
-  }
+    }
 }
 
 void ServoControl(String *arrayData) {
-  int RStickXValuePos = arrayData[4].toInt();
-  int RStickXValueNeg = arrayData[5].toInt();
-  int RStickYValuePos = arrayData[6].toInt();
-  int RStickYValueNeg = arrayData[7].toInt();
-  //XServo.writeMicroseconds(1500);
-  RStickXValuePos = map(RStickXValuePos, 0, 254, 90, 180);
-  RStickXValueNeg = map(RStickXValueNeg, 0, 255, 90, 0);
-
-  if (RStickXValuePos > 90){
+    /*Moving servos with the help of a library which talks to the servos through PWM*/
+    //Setting the values from array which is being provided through the serial-connection
+    int RStickXValuePos = arrayData[4].toInt();
+    int RStickXValueNeg = arrayData[5].toInt();
+    int RStickYValuePos = arrayData[6].toInt();
+    int RStickYValueNeg = arrayData[7].toInt();
+    //XServo.writeMicroseconds(1500);
+    //fitting the values from 0-255 to 0-180°
+    RStickXValuePos = map(RStickXValuePos, 0, 254, 90, 180);
+    RStickXValueNeg = map(RStickXValueNeg, 0, 255, 90, 0);
+    //Turning the servos
+    if (RStickXValuePos > 90){
      XServo.write(RStickXValuePos);
-  }
-  else if (RStickXValueNeg < 90){
+    }
+    else if (RStickXValueNeg < 90){
     XServo.write(RStickXValueNeg);
-  }
-  else{
+    }
+    else{
     XServo.write(90);
-  }             
+    }
 }
