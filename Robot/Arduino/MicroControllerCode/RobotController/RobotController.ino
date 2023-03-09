266d5e571fd072f3a0f5fbb6c198f0e0f3800aee
Uhgtred <markus_koesters@gmx.de>
2023-03-09 13:55:04 +0100

    Making code more readable and putting a test-line that is actually able to run the motors

--- Robot/Arduino/MicroControllerCode/RobotController/RobotController.ino
+++ Robot/Arduino/MicroControllerCode/RobotController/RobotController.ino
@@ -29,25 +29,29 @@ void setup() {
 }
 
 void loop() {
-    stringData = Serial.readString(); //"TEST,TEST2,TEST3"; Testing-line
-    /*Setting the motor-pins low in each iteration. If something gets stock or communication breaks robot will stop!*/
+    stringData = Serial.readString(); //"250,0,100,1"; //Testing-line
+    /*
+    Setting the motor-pins low in each iteration. If something gets stock or communication breaks robot will stop!
+    */
     digitalWrite(LMotorFPin, LOW);
     digitalWrite(LMotorRPin, LOW);
     digitalWrite(RMotorFPin, LOW);
     digitalWrite(RMotorRPin, LOW);
     if (stringData.length() != 0){
-    String *arrayData = ToStringArray(stringData);
-    Serial.println(arrayData[0] + arrayData[1]);
-    MotorControl(arrayData);
-    ServoControl(arrayData);
-    delete[] arrayData;
+        String *arrayData = ToStringArray(stringData);
+        Serial.println(arrayData[0] + arrayData[1]);
+        MotorControl(arrayData);
+        ServoControl(arrayData);
+        delete[] arrayData;
     }
     delay(50);
 }
 
 String *ToStringArray(String data){
-    /*Making array of comma-separated string!*/
-    /*Counting the number of separators to define array-length*/
+    /*
+    Making array of comma-separated string!
+    */
+    //Counting the number of separators to define array-length
     char separator;
     separator = ','; 
     int numberOfSeparators = 0;
@@ -56,12 +60,12 @@ String *ToStringArray(String data){
             numberOfSeparators++;
         }
     }
-    /*Defining Array-length and splitting the string into substrings which are bing stored in the array!*/
+    //Defining Array-length and splitting the string into substrings which are bing stored in the array!
     String *newData = new String[numberOfSeparators + 1];
     int lastPosition = 0;
     int counter = 0;
     for (int i=0; i <= data.length() - 1; i++){
-        // if a separator has been found, the string will be taken from last separator-position to the new position
+        //If a separator has been found, the string will be taken from last separator-position to the new position
         if (data[i] == separator){
             newData[counter] = data.substring(lastPosition, i);
             counter++;
@@ -72,7 +76,9 @@ String *ToStringArray(String data){
 }
 
 void MotorControl(String *arrayData){
-    /*Sending PWM-signals to the motor-controllers*/
+    /*
+    Sending PWM-signals to the motor-controllers
+    */
     int RMotorValue = 0;
     int LMotorValue = 0;
     //reading the data from array which is being provided through the serial-connection
@@ -80,26 +86,28 @@ void MotorControl(String *arrayData){
     LMotorValue = arrayData[2].toInt();
     //data[1] is bool and decides if RMotor is turning clockwise or counterclockwise
     if (arrayData[1].toInt() == 1){
-    analogWrite(RMotorRPin, RMotorValue);
-    //Serial.println(RMotorValue);
+        analogWrite(RMotorRPin, RMotorValue);
+        //Serial.println(RMotorValue);
     }
     else{
-    analogWrite(RMotorFPin, RMotorValue);
-    //Serial.println(RMotorValue);
+        analogWrite(RMotorFPin, RMotorValue);
+        //Serial.println(RMotorValue);
     }
     //data[3] is bool and decides if LMotor is turning clockwise or counterclockwise
     if (arrayData[3].toInt() == 1){
-    analogWrite(LMotorRPin, LMotorValue);
-    //Serial.println(LMotorValue);
+        analogWrite(LMotorRPin, LMotorValue);
+        //Serial.println(LMotorValue);
     }
     else{
-    analogWrite(LMotorFPin, LMotorValue);
-    //Serial.println(LMotorValue);
+        analogWrite(LMotorFPin, LMotorValue);
+        //Serial.println(LMotorValue);
     }
 }
 
 void ServoControl(String *arrayData) {
-    /*Moving servos with the help of a library which talks to the servos through PWM*/
+    /*
+    Moving servos with the help of a library which talks to the servos through PWM
+    */
     //Setting the values from array which is being provided through the serial-connection
     int RStickXValuePos = arrayData[4].toInt();
     int RStickXValueNeg = arrayData[5].toInt();
@@ -111,12 +119,12 @@ void ServoControl(String *arrayData) {
     RStickXValueNeg = map(RStickXValueNeg, 0, 255, 90, 0);
     //Turning the servos
     if (RStickXValuePos > 90){
-     XServo.write(RStickXValuePos);
+        XServo.write(RStickXValuePos);
     }
     else if (RStickXValueNeg < 90){
-    XServo.write(RStickXValueNeg);
+        XServo.write(RStickXValueNeg);
     }
     else{
-    XServo.write(90);
+        XServo.write(90);
     }
 }
