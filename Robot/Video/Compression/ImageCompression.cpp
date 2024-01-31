e0b61c17564fd3cf43e07cef289fbd84d2533a94
Uhgtred <Markus_koesters@gmx.de>
2024-01-31 22:04:30 +0100

    understanding opencv in c++

---
+++ Robot/Video/Compression/ImageCompression.cpp
@@ -0,0 +1,19 @@
+//
+// Created by markus on 31.01.24.
+//
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+#include "ImageCompression.h"
+
+namespace ImageCompression {
+class ImageCompression{
+
+    Mat compressImage(Mat image){
+
+    }
+};
+} // ImageCompression
---
+++ Robot/Video/Compression/ImageCompression.h
@@ -0,0 +1,16 @@
+//
+// Created by markus on 31.01.24.
+//
+
+#ifndef IMAGECOMPRESSION_H
+#define IMAGECOMPRESSION_H
+
+namespace ImageCompression {
+
+class ImageCompression {
+
+};
+
+} // ImageCompression
+
+#endif //IMAGECOMPRESSION_H
