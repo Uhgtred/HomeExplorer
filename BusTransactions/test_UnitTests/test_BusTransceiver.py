424fd50da8f1e49a11f2fef9b19dbbe40fb50cfb
Uhgtred <Markus_koesters@gmx.de>
2024-02-11 15:40:27 +0100

    Switching directories (#43)
    * switching directories
    
    * numpy and bytesio wont work together
    
    sadly it is not possible to get numpy.save working with bytesio, so i have to search for another solution probably going for simple file. If this is too slow, maybe have to switch to c++
    
    * Update SerializationNumpySave.py
    
    * Update main.yml
    
    * Update Dockerfile
    
    * Update main.yml
    
    * Update test_CameraConfig.py
    
    * Update test_CameraConfig.py

---
+++ BusTransactions/test_UnitTests/test_BusTransceiver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# @author: Markus Kösters
+
+import unittest
+
+from BusTransactions import BusFactory, SerialBus, SerialBusConfig
+from BusTransactions.BusPlugins.SerialBusModule.test_UnitTests.SerialBusMock import MockSerialBus
+from BusTransactions import Encoding
+
+
+class MyTestCase(unittest.TestCase):
+    transceiver = BusFactory.BusFactory()
+    config = SerialBusConfig('test', 123, MockSerialBus)
+    bus = SerialBus(config)
+    transceiver = transceiver.produceBusTransceiver(bus, Encoding.EncodingFactory.arduinoSerialEncoding)
+    testString = 'Test from BusTransceiver'
+    messages = []
+
+    def test_BusTransceiver_writeSingleMessage(self):
+        self.transceiver.writeSingleMessage(self.testString)
+        message = self.transceiver.bus.bus.buffer.pop(0)
+        self.assertEqual(message[:-1], self.testString.encode())
+
+    def test_BusTransceiver_readSingleMessage(self):
+        self.transceiver.writeSingleMessage(self.testString)
+        message = self.transceiver.readSingleMessage()
+        self.assertEqual(message, self.testString)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- Remote/Network/__init__.py
+++
@@ -1,1 +0,0 @@
-from .SocketController import SocketController
