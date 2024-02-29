6915b1c5cf28d45b0a25fa8ab1c82e96a7f54085
Uhgtred <Markus_koesters@gmx.de>
2024-02-29 12:26:01 +0100

    returning from set to list partially
    in iterative attempts sets are not a good idea, since they do not allow a modification during runtime

--- Runners/threadRunner.py
+++ Runners/threadRunner.py
@@ -11,7 +11,7 @@ class ThreadRunner(Runner):
     Method for organizing threads and keeping track of opened tracks.
     """
 
-    __threads: set = set()
+    __threads: list = []
     __running: bool = False
 
     def addTask(self, task, *args) -> None:
@@ -21,7 +21,7 @@ class ThreadRunner(Runner):
         :param args: Arguments, that shall be passed to the thread.
         """
         thread = threading.Thread(target=task, args=args, name=f'{str(task).split(" ")[1]}_thread')
-        self.__threads.add(thread)
+        self.__threads.append(thread)
 
     def runTasks(self) -> None:
         """
@@ -31,10 +31,7 @@ class ThreadRunner(Runner):
             return
         self.__running = True
         while self.__running and len(self.__threads) > 0:
-            for thread in self.__threads:
-                self.__threads.remove(thread)
-                print(self.__threads)
-                thread.start()
+            self.__threads.pop().start()
         self.__running = False
 
     def stopTasks(self) -> None:
