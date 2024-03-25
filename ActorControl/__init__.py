0d04668f87570fe93f158dfa69a23ee82ab085f5
Markus Kösters <Markus_koesters@gmx.de>
2024-03-25 13:31:51 +0100

    Update __init__.py

--- ActorControl/__init__.py
+++ ActorControl/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # @author: Markus Kösters
 
-from ActorController import ActorController
-from ControlDevice import ControlDevice
-from ActorControlInterface import ActorControlInterface
+from .ActorController import ActorController
+from .ControlDevice import ControlDevice
+from .ActorControlInterface import ActorControlInterface
