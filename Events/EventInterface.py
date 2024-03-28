23e34db6190caa5f4a3e0ed1a9d497fb2facd16c
Markus Kösters <Markus_koesters@gmx.de>
2024-03-28 14:37:37 +0100

    update architecture
    Updating the architecture and adapting the code.

--- Events/EventInterface.py
+++ Events/EventInterface.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python3
-# @author      Markus Kösters
+# @author: Markus Kösters
 
-from .Event import Event
-from .EventInterface import EventInterface
+from abc import ABC, abstractmethod
+
+from Events import Event
 
 
-class EventManager(EventInterface):
-    """
-    Factory-class for EventUser.
-    """
-
-    __events: dict = {}
+class EventInterface(ABC):
 
     @classmethod
+    @abstractmethod
     def produceEvent(cls, name: str) -> Event:
         """
         Method producing a new event.
         :return:    An instance of an Event, that can be used to create an update
                     for the subscribers and for subscribing to this event.
         """
-        # Only adds the key to the dictionary if it does not already exist!
-        cls.__events.setdefault(name, Event())
-        return cls.__events.get(name)
 
     @property
+    @abstractmethod
     def getEventsList(self) -> list[str]:
         """
         Getter Method for Events available.
         :return: List of available Events.
         """
-        return list(self.__events.keys())
