8a17873e4150682916a8037afc43a32ca8b66a37
Uhgtred <Markus_koesters@gmx.de>
2024-10-31 21:28:36 +0100

    Improving controls on Arduino.
    Improving the reaction-time, delay and stuck backwards direction.

--- Arduino/RobotController/RobotController.ino
+++ Arduino/RobotController/RobotController.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-#include "libraries/ArduinoJson/ArduinoJson.h"
+#include <ArduinoJson.h>
 
 /*
 Declaring and defining Pins
@@ -22,7 +22,7 @@ StaticJsonDocument<80> jsonDocument;
 struct actorMapDataType {
     String actorName;
     String actorID;
-}
+};
 
 void setup() {
     //Setting up serial parameters
@@ -45,13 +45,13 @@ void setup() {
 
 void loop() {
     //Setting the motor-pins low in each iteration. If something gets stuck or communication breaks robot will stop!
-    SetMotorsZero();
+    // SetMotorsZero();
     readSerialJson();
     //Sending transformed data to motors and servos
     MotorControl();
     //ServoControl();
     //Serial.println(String(serialByteData[0]) + ' ' + String(serialByteData[2]));
-    delay(50);
+    // delay(50);
 }
 
 int readJsonValue(String key){
@@ -69,8 +69,16 @@ void readSerialJson(){
 
 void SetMotorsZero(void){
     //Setting the motor-pins low in each iteration. If something gets stuck or communication breaks robot will stop!
+    SetLeftMotorsZero();
+    SetRightMotorsZero();
+}
+
+void SetLeftMotorsZero(void){
     digitalWrite(LMotorFPin, LOW);
     digitalWrite(LMotorRPin, LOW);
+}
+
+void SetRightMotorsZero(void){
     digitalWrite(RMotorFPin, LOW);
     digitalWrite(RMotorRPin, LOW);
 }
@@ -86,21 +94,27 @@ void MotorControl(){
     LMotorValue = readJsonValue("LeftMotor");
     //Motor turns clockwise if value is greater 0 else turns counterclockwise. Those are different pins though.
     if (RMotorValue < 0){
-        analogWrite(RMotorRPin, RMotorValue);
+        analogWrite(RMotorRPin, abs(RMotorValue));
 //        Serial.println(RMotorValue);  // debugging-line
     }
-    else{
+    else if(RMotorValue > 0){
         analogWrite(RMotorFPin, RMotorValue);
 //        Serial.println(RMotorValue);  // debugging-line
     }
+    else{
+        SetLeftMotorsZero();
+    }
     //data[3] is bool and decides if LMotor is turning clockwise or counterclockwise
     if (LMotorValue < 0){
-        analogWrite(LMotorRPin, LMotorValue);
+        analogWrite(LMotorRPin, abs(LMotorValue));
 //        Serial.println(LMotorValue);  // debugging-line
     }
-    else{
+    else if (LMotorValue > 0){
         analogWrite(LMotorFPin, LMotorValue);
 //        Serial.println(LMotorValue);  // debugging-line
+    }
+    else {
+      SetRightMotorsZero();
     }
 }
 
@@ -110,6 +124,7 @@ void ServoControl() {
     */
     //Setting the values from Json-document which is being provided through the serial-connection
     int CameraXValue = readJsonValue("CameraXServo");
+    long int RStickXValue;
     //fitting the values from -255 - 255 to 0-180°
     RStickXValue = map(RStickXValue, -254, 254, 0, 180);
     //Turning the servos
